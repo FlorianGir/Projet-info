5aeb75603a733e9775b6ccddfc038aa5b0732f32
FlorianGir <43701471+FlorianGir@users.noreply.github.com>
2018-10-10 19:39:05 +0200

    Update Fonction_liste.h

--- Fonction_liste.h
+++ Fonction_liste.h
@@ -4,19 +4,19 @@
 
 
 
-// Different types de lexemes obtenables
+//! Different types de lexemes obtenables
 typedef enum LEXEME {DEBUT, COMMENT, NL, SYMBOLE, DEUX_PTS, DIRECTIVE, VIRGULE, DECIMAL, DECIMAL_ZERO, HEXA, AUTRE, REGISTRE, GUIL, PAR_O, PAR_F} LEXEME; 
 
 
-// Liste chainee qui forme un mot propre a un lexeme 
+//! Liste chainee qui forme un mot propre a un lexeme 
 struct LETTRE {
-	int c;				// Un des caracteres du mot (ex : A de ADD)
-   	struct LETTRE* suiv; };		// Chainage
+	int c;				//! Un des caracteres du mot (ex : A de ADD)
+   	struct LETTRE* suiv; };		//! Chainage
 
 typedef struct LETTRE* MOT;
 
 
-//Liste chainee qui comporte un mot et son lexeme associe pour chaque bloc (ex : SYMBOLE --> mot possible associé : ADD)
+//!Liste chainee qui comporte un mot et son lexeme associe pour chaque bloc (ex : SYMBOLE --> mot possible associÃ© : ADD)
 struct ELEMENT {
    	LEXEME lex;
     	MOT mot;
@@ -25,13 +25,14 @@ struct ELEMENT {
 typedef struct ELEMENT* L_LEX;
 
 
-// Prototypes des fonctions utilisees 
+//! Prototypes des fonctions utilisees 
 L_LEX creer_liste_lex(void);
 MOT creer_liste_mot(void);
 L_LEX ajout_tete_lex(L_LEX l, LEXEME lex, MOT l_mot);
 L_LEX ajout_queue_lex(L_LEX l, LEXEME lex, MOT l_mot);
 MOT ajout_tete_mot(MOT l_mot, int c);
 MOT ajout_queue_mot(MOT l_mot, int c);
+L_LEX erreur_carac();
 void affiche_lex(L_LEX lex);
 void affiche_mot(L_LEX p_lex);
 void Virgule(L_LEX lex, MOT *mot);
