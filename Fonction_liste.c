ab3a7f2ba7de84b1b5e9a8fdfc749318c80f9f65
FlorianGir <43701471+FlorianGir@users.noreply.github.com>
2018-10-10 19:29:32 +0200

    Update Fonction_liste.c

--- Fonction_liste.c
+++ Fonction_liste.c
@@ -12,27 +12,26 @@ MOT creer_liste_mot(void) {
 }
 
 L_LEX ajout_tete_lex(L_LEX l_lex, LEXEME lex, MOT l_mot) {
-	L_LEX p = NULL;				// Création pointeur sur liste lexeme
-	p = calloc(1, sizeof(*p));		// Allocation de mémoire
-	p->lex = lex;				// On complète le bloc cree
+	L_LEX p = NULL;				//! CrÃ©ation pointeur sur liste lexeme
+	p = calloc(1, sizeof(*p));		//! Allocation de mÃ©moire
+	p->lex = lex;				//! On complÃ¨te le bloc cree
 	p->mot = l_mot;
-	p->suiv = l_lex;			// Liaison du bloc avec les suivants
- 	return p;				// On renvoie p pour ajouter le bloc en tete
+	p->suiv = l_lex;			//! Liaison du bloc avec les suivants
+ 	return p;				//! On renvoie p pour ajouter le bloc en tete
 }
-
 
 L_LEX ajout_queue_lex(L_LEX l_lex, LEXEME lex, MOT l_mot) {
 	L_LEX p = NULL;
 	p = calloc(1, sizeof(*p));
 	p->lex = lex;
 	p->mot = l_mot;
-	if (l_lex == NULL) return p;	// Si liste NULL, on a que ce bloc en memoire
-	else {				// Sinon, on parcour la liste pour arriver jusqu'au pointeur de fin 
+	if (l_lex == NULL) return p;	//! Si liste NULL, on a que ce bloc en memoire
+	else {				//! Sinon, on parcour la liste pour arriver jusqu'au pointeur de fin 
 		L_LEX q = l_lex;
 		while (q->suiv != NULL) {
 			q = q->suiv;
 		}
-		q->suiv = p;		// On rattache le bloc cree en fin de liste
+		q->suiv = p;		//! On rattache le bloc cree en fin de liste
 		return l_lex;
 	}
 }
@@ -63,7 +62,7 @@ MOT ajout_queue_mot(MOT l_mot, int c) {
 	}	
 }
 
-// Affichage des lexemes
+//! Affichage des lexemes
 void affiche_lex(L_LEX p_lex) {
 
 	if (p_lex->lex == DEBUT) printf("DEBUT --> ");
@@ -84,7 +83,7 @@ void affiche_lex(L_LEX p_lex) {
 	return;
 }
 
-// Affiche le mot associe a un lexeme
+//! Affiche le mot associe a un lexeme
 void affiche_mot(L_LEX p_lex) {
 	MOT q = p_lex->mot;
 	while (q != NULL) {
@@ -98,15 +97,15 @@ void affiche_mot(L_LEX p_lex) {
 }
 
 
-// Gestion erreur caractere : impose fin de fonction lect
+//! Gestion erreur caractere : impose fin de fonction lect
 L_LEX erreur_carac() {
 	printf("Erreur de caractere \n");	
 	return NULL;
 }
 
 
-// Gestion virgule 
-void Virgule(L_LEX lex, MOT* mot) {		// Passage de mot par adresse car modifie dans la fonction (pareil pour les 4 fonctions suivantes)
+//! Gestion virgule 
+void Virgule(L_LEX lex, MOT* mot) {		//! Passage de mot par adresse car modifie dans la fonction (pareil pour les 4 fonctions suivantes)
 	
 	*mot = NULL;
 	*mot = ajout_queue_mot(*mot, ',');
@@ -114,7 +113,7 @@ void Virgule(L_LEX lex, MOT* mot) {		// Passage de mot par adresse car modifie d
 	*mot = NULL;
 }
 
-// Gestion nouvelle ligne
+//! Gestion nouvelle ligne
 void New_Line(L_LEX lex, MOT* mot) {
 	
 	*mot = NULL;
@@ -123,7 +122,7 @@ void New_Line(L_LEX lex, MOT* mot) {
 	*mot = NULL;
 }
 
-//Gestion PAR_O
+//!Gestion PAR_O
 void Par_o(L_LEX lex, MOT *mot) {
 	
 	*mot = NULL;
@@ -132,7 +131,7 @@ void Par_o(L_LEX lex, MOT *mot) {
 	*mot = NULL;
 }
 
-//Gestion PAR_F
+//!Gestion PAR_F
 void Par_f(L_LEX lex, MOT* mot) {
 	
 	*mot = NULL;
@@ -141,7 +140,7 @@ void Par_f(L_LEX lex, MOT* mot) {
 	*mot = NULL;
 }
 
-//Gestion deux points
+//!Gestion deux points
 void Deux_pts(L_LEX lex, MOT *mot){
 	
 	*mot = NULL;
@@ -150,7 +149,7 @@ void Deux_pts(L_LEX lex, MOT *mot){
 	*mot = NULL;
 }
 
-/*//Gestion fin de nombre (virgule, espace, parenthèse...)
+/*!Gestion fin de nombre (virgule, espace, parenthÃ¨se...)
 void Fin_nbre(int c, L_LEX* lex, MOT* mot, LEXEME type_lexeme, LEXEME* A) {
 	
 	if (isspace(c)) {
@@ -177,12 +176,12 @@ void Fin_nbre(int c, L_LEX* lex, MOT* mot, LEXEME type_lexeme, LEXEME* A) {
 }
 */
 
-//Test des registres 
+//!Test des registres 
 int test_registre( int p, int u){
-	if ((isdigit(u))&&(isdigit(p))){						// Si le deuxieme carac est un nombre
-		if ((p == '0')||(p == '1')||(p == '2')) return 1;			// Registre existant entre 00 et 29
+	if ((isdigit(u))&&(isdigit(p))){						//! Si le deuxieme carac est un nombre
+		if ((p == '0')||(p == '1')||(p == '2')) return 1;			//! Registre existant entre 00 et 29
 		else if (p == '3'){
-			if ((u == '0')||(u == '1')) return 1;					// Registre 30 et 31
+			if ((u == '0')||(u == '1')) return 1;					//! Registre 30 et 31
 			else {
 				return 0;
 				printf("erreur de caractere pour les registres \n");
@@ -193,8 +192,8 @@ int test_registre( int p, int u){
 			printf("erreur de caractere pour les registres \n");
 		}
 	}
-	else if (isdigit(p)) return 1;									// Registre 0 a 9
-	else if ((p == 'a')&&(u == 't')) return 1;						// Mnemonique
+	else if (isdigit(p)) return 1;									//! Registre 0 a 9
+	else if ((p == 'a')&&(u == 't')) return 1;						//! Mnemonique
 	else if ((p == 'v')&&((u == '0')||(u == '1'))) return 1;
 	else if ((p == 'a')&&((u == '0')||(u == '1')||(u == '2')||(u == '3'))) return 1;
 	else if ((p == 't')&&((u == '0')||(u == '1')||(u == '2')||(u == '3')||(u == '4')||(u == '5')||(u == '6')||(u == '7'))) return 1;
@@ -211,9 +210,9 @@ int test_registre( int p, int u){
 		
 
 
-// TEST :
+/*! TEST :
 
-/*void main() {
+void main() {
 	
 	FILE* fichier;
 	MOT mot = NULL, p;
