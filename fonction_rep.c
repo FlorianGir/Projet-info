7e62c97ef5eabe3848b290778c671b7adebd6587
FlorianGir <43701471+FlorianGir@users.noreply.github.com>
2018-10-29 22:02:46 +0100

    Update fonction_rep.c

--- fonction_rep.c
+++ fonction_rep.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+
 #include <stdlib.h>
 #include <string.h>
+
 #include <math.h>
 #include <ctype.h>
 
@@ -12,579 +14,424 @@
 
 
 
-void repartition(L_LEX l_lex, L_INST l_inst, L_DATA l_data, L_BSS l_bss, L_SYMB l_symb){
+L_INST ajout_queue_inst(L_INST l_inst, MOT mot, LEXEME lex, int nb_op, int ligne, int decalage, L_LEX operande) {
 
-	
+	L_INST p = NULL;
 
-	L_LEX p, attente_op;
+	p = calloc(1, sizeof(*p));
 
-	L_INST q;
+	p->mot = mot;
+	p->lex = lex;
 
-	ETAT A = INIT;
+	p->nb_op = nb_op;
 
-	ETAT A_memory;
+	p->ligne = ligne;
 
-	char* c;
-	char tab[100];
+	p->decalage = decalage;
 
-	int existe, verif_cond;
+	p->operande = operande;
 
-	
+	if (l_inst == NULL) return p;	//! Si liste NULL, on a que ce bloc en memoire
 
-	int dec_bss = 0;			//! Decalages 
+	else {				//! Sinon, on parcour la liste pour arriver jusqu'au pointeur de fin 
 
-	int dec_text = 0;
+		L_INST q = l_inst;
 
-	int dec_data = 0;
+		while (q->suiv != NULL) {
 
-
-
-
-
-	attente_op = NULL;
-
-	p = l_lex;
-
-	while (p != NULL) {
-
-		
-
-		switch(A) {
-
-			
-
-		case INIT:
-
-		
-
-			recup_mot(p->mot, tab);				//! recuperation du mot associé au premier lexeme
-
-			
-
-			if (strcmp(c, ".text") == 0) A = TEXT;
-
-			else if (strcmp(c, ".data") == 0) A = DATA;
-
-			else if (strcmp(c, ".bss") == 0) A = BSS;
-
-			p = p->suiv;
-
-			break;
-
-			
-
-			
-
-		case TEXT:				//! quand on est dans .text 
-
-			
-
-			recup_mot(p->mot, tab);
-
-			
-
-			if (strcmp(c, ".data") == 0) A = DATA, p = p->suiv;
-
-			else if (strcmp(c, ".bss") == 0) A = BSS, p = p->suiv;
-
-			else if (p->suiv->mot->c == ':') A = TEXT_COLLE_SYMB;   //! 	LES IF S EFFECTUENT PAR ORDRE D ECRITURE ?
-
-			else if (p->lex == SYMBOLE) A = TEXT_SYMB;
-
-			else A = TEXT, p = p->suiv;
-
-			break;
-
-			
-
-			
-
-		case DATA:				//! quand on est dans .data
-
-		
-
-			recup_mot (p->mot, tab);
-
-			
-
-			if (strcmp(c, ".text") == 0) A = TEXT, p = p->suiv;
-
-			else if (strcmp(c, ".bss") == 0) A = BSS, p = p->suiv;
-
-			else if (p->lex == DIRECTIVE) A = DATA_DIRECTIVE;
-
-			else if (p->suiv->mot->c == ':') A = DATA_COLLE_SYMB;
-
-			break;
-
-		
-
-		
-
-		case BSS:				//! quand on est dans .bss
-
-		
-
-			recup_mot (p->mot, tab);
-
-			
-
-			if (strcmp(c, ".text") == 0) A = TEXT;
-
-			else if (strcmp(c, ".data") == 0) A = DATA;
-
-			else if (strcmp(c,".space") == 0) A = BSS_SPACE;
-
-			p = p->suiv;
-
-			break;
-
-		
-
-		
-
-		case TEXT_SYMB:				//! on est dans .text et on a un symbole
-
-			
-
-			existe = symb_existe(p->mot);	
-
-
-
-			if (existe == -1) {
-
-				printf("Ligne %d: erreur d'instruction dans la partie text \n", p->ligne);
-
-				return;
-
-			}
-
-			else {
-
-				A = TEST_OP ;
-
-				l_inst = ajout_queue_inst(l_inst, p->mot, SYMBOLE, existe, p->ligne, DECALAGE, NULL);  //! SYMBOLE ? + gérer decalage
-
-			}
-
-			p = p->suiv;
-
-			break;
-
-
-
-
-
-		case TEXT_COLLE_SYMB:
-
-	
-
-			l_symb = ajout_queue_symbole(l_symb, p->mot, p->ligne, ".text", decalage); //! TRAITER DECALAGE RELATIF
-
-			A = TEXT;
-
-			p = p->suiv;
-
-			break;
-
-		
-
-			
-
-		case TEST_OP: 			//! le symbole existe, test des operandes
-
-			
-
-			if ((existe > 1)&&(p->lex == (REGISTRE||DECIMAL||DECIMAL_ZERO||HEXA))){			//! lorsqu il y a encore plus de une operande
-
-				attente_op = ajout_queue_lex(attente_op, p->lex, p->mot, p->ligne);
-
-				A = SEPA_OP;
-
-				existe-- ;
-
-			}
-
-			else if ((existe == 1)&&(p->lex == (REGISTRE||DECIMAL||DECIMAL_ZERO||HEXA))){		//! lorsqu il reste que une operande
-
-				attente_op = ajout_queue_lex(attente_op, p->lex, p->mot, p->ligne);
-
-				q = l_inst;
-
-				while(q->suiv != NULL){
-
-					q = q->suiv;
-
-				}
-
-				q->operande = attente_op;			//! revoir vu que le maillon est en queue
-
-				attente_op = NULL;
-
-				A = TEXT;
-
-			}
-
-			else if (existe == 0){				        //! lorsqu il n y a pas d operande
-
-				A = TEXT;
-
-			}
-
-			else {
-
-				printf("Ligne %d: erreur dans les operandes dans une instruction de la partie text\n", p->ligne);
-
-				return;
-
-			}
-
-			p = p->suiv;
-
-			break;
-
-		
-
-		
-
-		case SEPA_OP:						//! test du separateur d'operande
-
-			
-
-			if ( p->lex == (VIRGULE)) A = TEST_OP;
-
-			else {
-
-				printf("Ligne %d: erreur de separateur dans une instruction de la partie text \n", p->ligne);
-
-				return;
-
-			}
-
-			p = p->suiv;
-
-			break;
-
-			
-
-
-
-		case DATA_DIRECTIVE:					//! on est dans .data et on a une directive
-
-		
-
-			if (strcmp(c, ".byte") == 0) A = DATA_BYTE;
-
-			else if (strcmp(c, ".word") == 0) A = DATA_WORD;
-
-			else if (strcmp(c, ".asciiz") == 0) A = DATA_ASCIIZ;
-
-			else if (strcmp(c, ".space") == 0) A = DATA_SPACE;
-
-			else {
-
-				printf("Ligne %d: erreur de directive inexistante dans la partie data \n", p->ligne);
-
-				return;
-
-			}
-
-			A_memory = A;
-
-			p = p->suiv;
-
-			break;
-
-		
-
-		
-
-		case DATA_BYTE:					//! cas ou la directive est .byte
-
-		
-
-			if ( p->lex == (DECIMAL||DECIMAL_ZERO||HEXA)) {
-
-				verif_cond = verification_word_byte(p->mot, p->lex, A);		//! fonction qui verifie les conditions operande de byte, renvoie 0 si c'est bon et 1 si non
-
-				if (verif_cond == 0){
-
-					dec_data += 1;			//! Decalage de 1 pour le .byte
-
-					attente_op = ajout_queue_lex(attente_op, p->lex, p->mot, p->ligne);
-
-					l_data = ajout_queue_data(l_data, ".byte", DIRECTIVE, dec_data, p->ligne, attente_op); //! NOMBRE OPERANDE ?
-
-					attente_op = NULL;
-
-					A = DATA_CHECK_SEPA;
-
-				}
-
-				else {
-
-					printf("Ligne %d: erreur d'operande dans le .byte de la partie data \n", p->ligne);
-
-					return;
-
-				}
-
-			}
-
-			else {
-
-				printf("Ligne %d: erreur d'operande dans la partie data \n", p->ligne);
-
-				return;
-
-			}
-
-			p = p->suiv;
-
-			break;
-
-			
-
-		
-
-		case DATA_WORD:				//! cas ou la directive est .word
-
-		
-
-			if ( p->lex == (DECIMAL||DECIMAL_ZERO||HEXA)) {
-
-				verif_cond = verification_word_byte(p->mot, p->lex, A);				//! creer fonction qui verifie les conditions operande de word, renvoie 0 si c'est bon et 1 si non
-
-				if (verif_cond == 0){
-
-					dec_data += 4;			//! Decalage de 4 pour le .word
-
-					attente_op = ajout_queue_lex(attente_op, p->lex, p->mot, p->ligne);
-
-					l_data = ajout_queue_data(l_data,".word", DIRECTIVE, 1, p->ligne, dec_data, attente_op);
-
-					attente_op = NULL;
-
-					A = DATA_CHECK_SEPA;
-
-				}
-
-				else {
-
-					printf("Ligne %d: erreur d'operande dans le .word de la partie data \n", p->ligne);
-
-					return;
-
-				}
-
-			}
-
-			else {
-
-				printf("Ligne %d: erreur d'operande dans la partie data \n", p->ligne);
-
-				return;
-
-			}
-
-			p = p->suiv;
-
-			break;
-
-			
-
-			
-
-		case DATA_ASCIIZ:			//! cas ou la directive est .asciiz
-
-		
-
-			if ( p->lex == GUIL) {
-
-				dec_data += nbre_cara(p->mot) - 1; 
-
-				attente_op = ajout_queue_lex(attente_op, p->lex, p->mot, p->ligne);
-
-				l_data = ajout_queue_data( l_data, ".asciiz", DIRECTIVE, 1, p->ligne, dec_data, attente_op);
-
-				attente_op = NULL;
-
-				A = DATA_CHECK_SEPA;
-
-			}
-
-			else {
-
-				printf("Ligne %d: erreur d'operande dans la partie data \n", p->ligne);
-
-				return;
-
-			}
-
-			p = p->suiv;
-
-			break;
-
-			
-
-		
-
-		case DATA_SPACE:			//! cas ou la directive est .space
-
-		
-
-			if ( p->lex == (DECIMAL||DECIMAL_ZERO||HEXA)) {
-
-				dec_data += valeur_deci(p->mot, p->lex);			//! Decalage de c pour le .space
-
-				attente_op = ajout_queue_lex(attente_op, p->lex, p->mot, p->ligne);
-
-				l_data = ajout_queue_data( l_data, ".space", DIRECTIVE, 1, p->ligne, dec_data, attente_op);
-
-				attente_op = NULL;
-
-				A = DATA_CHECK_SEPA;
-
-			}
-
-			else {
-
-				printf("Ligne %d: erreur d'operande dans la partie data \n", p->ligne);
-
-				return;
-
-			}
-
-			p = p->suiv;
-
-			break;
-
-		
-
-		
-
-		case DATA_CHECK_SEPA:
-
-		
-
-			if ( p->lex == (VIRGULE)) {
-
-				A = A_memory;
-
-			}
-
-			else if ( p->lex == (NL||COMMENT)){
-
-				A = DATA;
-
-			}
-
-			else {
-
-				printf("Ligne %d: erreur de separateur dans la partie data \n", p->ligne);
-
-				return;
-
-			}
-
-			p = p->suiv;
-
-			break;
-
-		
-
-		case DATA_COLLE_SYMB: 
-
-		
-
-			
-
-			l_symb = ajout_queue_symbole(l_symb, p->mot, p->ligne, ".data", decalage); //! TRAITER DECALAGE RELATIF
-
-			A = DATA;
-
-			p = p->suiv;
-
-			break;
-
-		
-
-		
-
-		case BSS_SPACE:				//! on est dans .bss et on a un .space
-
-			
-
-			if ( p->lex == (DECIMAL||DECIMAL_ZERO||HEXA)) {
-
-				dec_bss += valeur_deci(p->mot, p->lex);   
-
-				attente_op = ajout_queue_lex(attente_op, p->lex, p->mot, p->ligne);
-
-				l_bss = ajout_queue_bss( l_bss, ".space", DIRECTIVE, 1, p->ligne, DECALAGE, attente_op);
-
-				attente_op = NULL;
-
-				A = BSS_CHECK_SEPA;
-
-			}
-
-			else {
-
-				printf("Ligne %d: erreur d'operande dans la partie bss \n", p->ligne);
-
-				return;
-
-			}
-
-			p = p->suiv;
-
-			break;
-
-			
-
-		
-
-		case BSS_CHECK_SEPA:
-
-			
-
-			if ( p->lex == (VIRGULE)) {
-
-				A = BSS_SPACE;
-
-			}
-
-			else if ( p->lex == (NL||COMMENT)){
-
-				A = BSS;
-
-			}
-
-			else {
-
-				printf("Ligne %d: erreur de separateur dans la partie bss \n", p->ligne);
-
-				return;
-
-			}
-
-			p = p->suiv;
-
-			break;
-
-		
+			q = q->suiv;
 
 		}
+
+		q->suiv = p;		//! On rattache le bloc cree en fin de liste
+
+		return l_inst;
+
+	}
+
+}
+
+
+
+
+
+L_DATA ajout_queue_data(L_DATA l_data, char mot[20], LEXEME lex, int nb_op, int ligne, int decalage, L_LEX operande) {
+
+	L_DATA p = NULL;
+
+	p = calloc(1, sizeof(*p));
+
+	strcpy(p->mot, mot);
+	p->lex = lex;
+
+	p->nb_op = nb_op;
+
+	p->ligne = ligne;
+
+	p->decalage = decalage;
+
+	p->operande = operande;
+
+	if (l_data == NULL) return p;	//! Si liste NULL, on a que ce bloc en memoire
+
+	else {				//! Sinon, on parcour la liste pour arriver jusqu'au pointeur de fin 
+
+		L_DATA q = l_data;
+
+		while (q->suiv != NULL) {
+
+			q = q->suiv;
+
+		}
+
+		q->suiv = p;		//! On rattache le bloc cree en fin de liste
+
+		return l_data;
+
+	}
+
+}
+
+
+
+L_BSS ajout_queue_bss(L_BSS l_bss, char mot[20], LEXEME lex, int nb_op, int ligne, int decalage, L_LEX operande) {
+
+	L_BSS p = NULL;
+
+	p = calloc(1, sizeof(*p));
+
+	strcpy(p->mot, mot);
+	p->lex = lex;
+
+	p->nb_op = nb_op;
+
+	p->ligne = ligne;
+
+	p->decalage = decalage;
+
+	p->operande = operande;
+
+	if (l_bss == NULL) return p;	//! Si liste NULL, on a que ce bloc en memoire
+
+	else {				//! Sinon, on parcour la liste pour arriver jusqu'au pointeur de fin 
+
+		L_BSS q = l_bss;
+
+		while (q->suiv != NULL) {
+
+			q = q->suiv;
+
+		}
+
+		q->suiv = p;		//! On rattache le bloc cree en fin de liste
+
+		return l_bss;
+
+	}
+
+}
+
+
+
+
+
+L_SYMB ajout_queue_symb(L_SYMB l_symb, MOT mot, int ligne, char section[20], int decalage) {
+
+	L_SYMB p;
+
+	p = calloc(1, sizeof(*p));
+
+	p->mot = mot;
+
+	p->ligne = ligne;
+
+	strcpy(p->section, section);
+
+	p->decalage = decalage;
+
+	if (l_symb == NULL) return NULL;	//! Si liste NULL, on a que ce bloc en memoire
+
+	else {				//! Sinon, on parcours la liste pour arriver jusqu'au pointeur de fin 
+
+		L_SYMB q = l_symb;
+
+		while (q->suiv != NULL) {
+
+			q = q->suiv;
+
+		}
+
+		q->suiv = p;		//! On rattache le bloc cree en fin de liste
+
+		return l_symb;
+
+	}
+
+}
+
+
+
+
+
+
+
+void recup_mot(MOT mot, char tab[]){		//! Recupere un mot et fait une chaine de caractere avec, placee dans tab
+
+
+
+	MOT q = mot;
+
+	int a = 0;
+
+	if ( q == NULL) {
+
+		tab[0] = '\0';
+
+	}
+
+	else {		
+
+		while((q != NULL) && (a < 99)){
+
+			tab[a] = q->c;
+
+			q = q->suiv;
+
+			a++;
+
+		}
+
+		tab[a]='\0';
 
 	}
 
 	return;
 
+}
 
+	
+
+
+
+
+
+
+
+int symb_existe(MOT mot){		//! regarde si la directive existe et renvoie son nombre d'operande si oui et -1 sinon
+
+
+
+	int let_fich, test;
+
+	FILE* fich;
+
+	MOT q;
+
+	fich = fopen("dico.txt", "r");
+
+	if (fich == NULL) perror("Erreur ouverture fichier");	//! Si erreur ouverture
+
+	
+
+	test = 1;
+
+	while ((let_fich = fgetc(fich)) != EOF) {	//! Tant que pas a la fin du fichier
+
+		test = 1;
+
+		q = mot;
+
+		if (let_fich == toupper(q->c)){		//! touuper(...) --> add équivalent a ADD
+
+			while ( q->suiv != NULL){
+
+				q = q->suiv;
+
+				let_fich = fgetc(fich);
+
+				if (let_fich == toupper(q->c)) test = test*1;
+
+				else test = test*0;
+
+			}
+
+			if (test == 1) return fgetc(fich);
+
+			else while (let_fich != '\n') let_fich = fgetc(fich);
+
+
+
+		}
+
+		else {
+
+			while (let_fich != '\n') let_fich = fgetc(fich);
+
+		}
+
+	}
+
+	return -1;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int verification_word_byte(MOT mot, LEXEME lex, ETAT A) {		 //!fonction qui verifie les conditions operande de word et byte, renvoie 0 si c'est bon et 1 si non
+
+	int a;
+	
+	if ((lex == DECIMAL) || (lex = HEXA)){				//! Si nombre Hexa ou Deci
+		a = valeur_deci(mot, lex);				//! Conversion de la valeur en entier
+		
+		if (A == DATA_WORD) {					//! Si on est dans la section .data
+			if ((a < 2147483647) && (a > -2147483648)) return 0;		//! Condition de validite
+			else return 1;
+		
+		}
+		else if (A == DATA_BYTE) {				//! Si on est dans la section .byte
+			if ((a < 127) && (a > -128)) return 0;		//! Condition de validite
+			else return 1;
+	
+		}
+	}
+
+	else if (lex == DECIMAL_ZERO) return 0;	
+	
+}
+
+
+
+
+
+
+
+//! Compte le nombre de caractère d'un mot asciiz en comptant les  guillemets, il suffit de retrancher 1 pour prendre en compte uniquement les caractères et le '\0' de fin
+
+int nbre_cara(MOT mot) {
+
+	
+
+	int compteur = 0;
+
+	MOT q = mot;
+
+	while (q != NULL) {
+
+		compteur++;
+
+		q = q->suiv;
+
+	}
+
+	return compteur;
+
+}
 
 		
 
+	
+
+//! Convertit une chaine de caractere decimale ou sous forme hexa en valeur decimale
+
+int valeur_deci(MOT mot, LEXEME lex) {
+
+	char tab[100];
+
+	int i = 0;
+
+	int valeur = 0;
+
+	int nbre_chiffre_hexa;
+
+	char deci_int[2];
+
+	MOT q = mot;
+
+	
+
+	if (lex == DECIMAL) {			//! lex == DECIMAL
+
+		recup_mot(mot, tab);
+
+		return atoi(tab);		//! On convertit simplement la chaine de caractere en decimal
+
+	}
+
+
+
+	else if (lex == HEXA) {			//! lex == HEXA
+
+		
+
+		recup_mot(mot,tab);
+
+		
+
+		while (tab[i+2] != '\0') {	//! Compteur de caractere intervenant dans le calcul de la valeur decimale (sans le 0x devant)
+
+			i++;
+
+		}
+
+
+
+		nbre_chiffre_hexa = i;
+
+		
+
+		q = q->suiv;
+
+		for (i = 0; i < nbre_chiffre_hexa; i++) {	//! Calcul de la valeur en fonction du caractere hexadecimal
+
+			q = q->suiv;				//! q commence a pointer sur le caractère après le 0x
+
 			
 
+			if (isdigit(tab[i+2])) {		//! Si c'est un chiffre
+
+				deci_int[0] = q->c;
+
+				valeur += (atoi(deci_int))*pow(16, nbre_chiffre_hexa - i - 1);
+
+			}
+
+
+
+			//! Si c'est une lettre (entre a et f, minuscule ou majuscule)
+
+			else if (tolower(tab[i+2]) == 'a') valeur += 10*pow(16, nbre_chiffre_hexa - i - 1);
+
+			else if (tolower(tab[i+2]) == 'b') valeur += 11*pow(16, nbre_chiffre_hexa - i - 1);	
+
+			else if (tolower(tab[i+2]) == 'c') valeur += 12*pow(16, nbre_chiffre_hexa - i - 1);
+
+			else if (tolower(tab[i+2]) == 'd') valeur += 13*pow(16, nbre_chiffre_hexa - i - 1);
+
+			else if (tolower(tab[i+2]) == 'e') valeur += 14*pow(16, nbre_chiffre_hexa - i - 1);
+
+			else if (tolower(tab[i+2]) == 'f') valeur += 15*pow(16, nbre_chiffre_hexa - i - 1);
+
+		}
+
+		return valeur;
+
+	}
+
+
+
+	else return 0;		//! lex == DECIMAL_ZERO
+
 }
+
+	
+
